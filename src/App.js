ea1bd8180b0db9b51f03278e1b8f3df06ff31b9a
Justin Goncalves <jvgoncalves85@gmail.com>
2022-04-11 16:17:20 -0400

    Added Axios + deleted 6 files + created song data on Frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
 
 function App() {
+
+  const [songs, setSongs] = useState([])
+
+  useEffect(() => {
+    getAllSongs();
+  }, [])
+
+async function getAllSongs(){
+  let response = await axios.get('http://127.0.0.1:8000/api/music/');
+  setSongs(response.data);
+
+}
+
+async function createSong(newSong){
+  let response = await axios.post('http://127.0.0.1:8000/api/music/', newSong);
+  if(response.status === 201) {
+    await getAllSongs();
+  }
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Artist</th>
+            <th>Album</th>
+            <th>Genre</th>
+            <th>Release Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {songs.map((song) => {
+            return (
+              <tr>
+                <td>{song.title}</td>
+                <td>{song.artist}</td>
+                <td>{song.album}</td>
+                <td>{song.genre}</td>
+                <td>{song.release_date}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+
     </div>
   );
 }
