759ef71f72364bdd1c15686a13f1cdde2f16e17f
Justin Goncalves <jvgoncalves85@gmail.com>
2022-05-02 19:08:54 -0400

    Adjusted searchbar + colored add button

--- src/Components/Songs/DisplaySongs.jsx
+++ src/Components/Songs/DisplaySongs.jsx
@@ -1,8 +1,7 @@
 import React from "react";
-
 function DisplaySongs(props) {
   return (
-    <table className="table">
+    <table className="table" >
       <thead>
         <tr>
           <th>#</th>
